19fe507039b527dbb6d877fc26d8844d2ee09fde
Piotr B <b.piotr@gmail.com>
2015-03-11 23:10:10 +0000

    Added index and show page for recipes

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,12 +1,11 @@
 $(document).on('ready page:load', function () {
- 
-    $(".scroll a, .navbar-brand, .gototop,.explore").click(function(event){   
-    event.preventDefault();
-    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 600,'swing');
-    $(".scroll li").removeClass('active');
-    $(this).parents('li').toggleClass('active');
+    $(".scroll a, .navbar-brand-clik, .gototop,.explore").click(function(event){   
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 600,'swing');
+        $(".scroll li").removeClass('active');
+        $(this).parents('li').toggleClass('active');
     });
-    });
+});
 
 
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def gravatar_for(email, options = { size: 80 })
+    gravatar_id = Digest::MD5::hexdigest(email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+  end
 end
