6eaa66923558e23da4079b576c45eb2e3a852edb
Piotr B <b.piotr@gmail.com>
2015-03-13 14:53:21 +0000

    Updated to rails 4.2.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  get 'chefs/new'
-
   get 'pages/home'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'pages#home'
-
+  # root 'welcome#index'
+  root 'pages#home'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
@@ -26,7 +23,6 @@ Rails.application.routes.draw do
   resources :chefs, except: [:new]
   
   get '/register' => 'chefs#new'
-
   # Example resource route with options:
   #   resources :products do
   #     member do
